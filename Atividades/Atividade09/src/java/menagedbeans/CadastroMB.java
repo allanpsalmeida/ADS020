87f912a92acc3387d0b4a250928e284d547469a5
allanpsalmeida <53920924+allanpsalmeida@users.noreply.github.com>
2020-04-23 00:34:08 -0300

    Add files via upload

---
+++ Atividades/Atividade09/src/java/menagedbeans/CadastroMB.java
@@ -0,0 +1,20 @@
+
+package menagedbeans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+
+@ManagedBean
+@RequestScoped
+public class CadastroMB {
+
+    
+    public CadastroMB() {
+    }
+    
+    public String salvar(){
+    
+        return "lista";
+    }
+}
