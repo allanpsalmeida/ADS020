be54cadbd9f64269dd067a7b7fb2a2fa0075926f
allanpsalmeida <53920924+allanpsalmeida@users.noreply.github.com>
2020-04-23 00:31:56 -0300

    Add files via upload

---
+++ Atividades/Atividade06/src/java/filtro/LoginFilter.java
@@ -0,0 +1,66 @@
+package filtro;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebFilter(filterName = "LoginFilter", urlPatterns = {"/*"})
+public class LoginFilter implements Filter {
+
+    private static final boolean debug = true;
+
+    private FilterConfig filterConfig = null;
+
+    public LoginFilter() {
+    }
+
+    public void doFilter(ServletRequest request, ServletResponse response,
+            FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse resp = (HttpServletResponse) response;
+        HttpSession session = req.getSession();
+
+        if (session.getAttribute("autenticado") != null
+                || req.getRequestURI().equals(req.getContextPath() + "/login")) {
+            chain.doFilter(req, resp);
+        } else {
+            resp.sendRedirect(req.getContextPath() + "/login");
+        }
+    }
+
+    public void destroy() {
+    }
+
+    public void init(FilterConfig filterConfig) {
+        this.filterConfig = filterConfig;
+        if (filterConfig != null) {
+            if (debug) {
+
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        if (filterConfig == null) {
+            return ("LoginFilter()");
+        }
+        StringBuffer sb = new StringBuffer("LoginFilter(");
+        sb.append(filterConfig);
+        sb.append(")");
+        return (sb.toString());
+    }
+
+}
